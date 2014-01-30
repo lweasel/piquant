350986509627efbd97f42092e95621b27299aca6
Owen Dando <owen.dando@ed.ac.uk>
2014-01-30 13:52:16 +0000

    Validate provided Flux Simulator expression profile file can be opened.

--- set_isoform_frequencies.py
+++ set_isoform_frequencies.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
 """Usage:
-    set_isoform_frequencies [{help}] [{version}]
+    set_isoform_frequencies [{help}] [{version}] {pro_file}
 
 {help_short} {help}                 Show this message.
 {version_short} {version}              Show version.
+{pro_file}  Flux simulator gene expression profile file.
 """
 
 from docopt import docopt
@@ -14,19 +15,28 @@ HELP_SHORT = "-h"
 HELP = "--help"
 VERSION_SHORT = "-v"
 VERSION = "--version"
+PRO_FILE = "<pro-file>"
 
 __doc__ = __doc__.format(
     help_short=HELP_SHORT,
     help=HELP,
     version_short=VERSION_SHORT,
-    version=VERSION)
+    version=VERSION,
+    pro_file=PRO_FILE)
 
 # Read in command-line options
 options = docopt(__doc__, version="set_isoform_frequencies v0.1")
 
+
 # Validate command-line options
+
+def validate_file_option(file_option, msg):
+    msg = "{msg} '{file}'.".format(msg=msg, file=file_option)
+    return Schema(open, error=msg).validate(file_option)
+
 try:
-    pass
+    options[PRO_FILE] = validate_file_option(
+        options[PRO_FILE], "Could not open expression profile file")
 except SchemaError as exc:
     exit(exc.code)
 
