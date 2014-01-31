a340c8a20ec757ae62bf0edfd0bcc59f0909c727
Owen Dando <owen.dando@ed.ac.uk>
2014-01-31 14:50:01 +0000

    Read GTF file using genes_to_gtf.

--- assess_isoform_quantification/set_isoform_frequencies.py
+++ assess_isoform_quantification/set_isoform_frequencies.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python
 
 """Usage:
-    set_isoform_frequencies [{help}] [{version}] [{log_level}={log_level_val}] {pro_file}
+    set_isoform_frequencies [{help}] [{version}] [{log_level}={log_level_val}] [{out_dir}={out_dir_val}] {pro_file} {gtf_file}
 
 {help_short} {help}                 Show this message.
 {version_short} {version}              Show version.
-{log_level}={log_level_val}   Set logging level (one of {log_level_vals}) [default: 'info'].
+{log_level}={log_level_val}   Set logging level (one of {log_level_vals}) [default: info].
+{out_dir}={out_dir_val}   Directory for output files; will be created if it doesn't exist [default: .].
 {pro_file}                Flux simulator gene expression profile file.
+{gtf_file}                GTF file defining genes and transcripts.
 """
 
 from docopt import docopt
+from gtf_to_genes import index_gtf_files, get_indexed_genes_for_identifier
 from log import getLogger, LEVELS
 from options import validate_file_option, validate_dict_option
 from pandas import read_csv
 from schema import SchemaError
 from sys import stderr
 
+import os.path
+
 HELP_SHORT = "-h"
 HELP = "--help"
 VERSION_SHORT = "-v"
 VERSION = "--version"
 PRO_FILE = "<pro-file>"
+GTF_FILE = "<gtf-file>"
 LOG_LEVEL = "--log-level"
 LOG_LEVEL_VAL = "<log-level>"
 LOG_LEVEL_VALS = str(LEVELS.keys())
+OUT_DIR = "--output-dir"
+OUT_DIR_VAL = "<output-dir>"
 
 __doc__ = __doc__.format(
     help_short=HELP_SHORT,
@@ -31,9 +39,12 @@ __doc__ = __doc__.format(
     version_short=VERSION_SHORT,
     version=VERSION,
     pro_file=PRO_FILE,
+    gtf_file=GTF_FILE,
     log_level=LOG_LEVEL,
     log_level_val=LOG_LEVEL_VAL,
-    log_level_vals=LOG_LEVEL_VALS)
+    log_level_vals=LOG_LEVEL_VALS,
+    out_dir=OUT_DIR,
+    out_dir_val=OUT_DIR_VAL)
 
 LOCUS_COL = 'loc'
 TRANSCRIPT_ID_COL = 't_id'
@@ -61,6 +72,8 @@ options = docopt(__doc__, version="set_isoform_frequencies v0.1")
 try:
     options[PRO_FILE] = validate_file_option(
         options[PRO_FILE], "Could not open expression profile file")
+    validate_file_option(
+        options[GTF_FILE], "Could not open GTF file")
     validate_dict_option(
         options[LOG_LEVEL], LEVELS, "Invalid log level")
 except SchemaError as exc:
@@ -74,3 +87,18 @@ transcript_abundances = read_csv(options[PRO_FILE], sep='\s*',
                                  index_col=TRANSCRIPT_ID_COL)
 
 num_molecules = transcript_abundances[NUM_TRANSCRIPTS_COL].sum()
+
+logger.info("Reading genes and transcripts from GTF file.")
+
+if not os.path.isdir(options[OUT_DIR]):
+    os.makedirs(options[OUT_DIR])
+
+index_file = options[OUT_DIR] + "/gtf.index"
+gtf_path, gtf_filename = os.path.split(options[GTF_FILE])
+
+if not os.path.exists(index_file):
+    index_gtf_files(index_file, gtf_path, r"(.+\/)(" + gtf_filename + ")$",
+                    r"\1/\2.cache", r"\2", False, logger)
+
+species, fn, genes = get_indexed_genes_for_identifier(index_file,  logger,  gtf_filename)
+logger.info(len(genes['protein_coding']))
