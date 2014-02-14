4628ca3866d2fff7c5b2d2ea5a3a7fbc4bb1dd8b
Owen Dando <owen.dando@ed.ac.uk>
2014-02-14 12:43:22 +0000

    Extract FPKM values from Cufflinks output file "isoforms.fpkm_tracking".

--- assess_isoform_quantification/quantifiers.py
+++ assess_isoform_quantification/quantifiers.py
@@ -1,6 +1,11 @@
+import pandas as pd
+
+
 class Cufflinks:
     def calculate_transcript_abundances(self, quant_file):
-        pass
+        self.abundances = pd.read_csv(quant_file, delim_whitespace=True,
+                                      index_col="tracking_id")
 
     def get_transcript_abundance(self, transcript_id):
-        return 0
+        return self.abundances.ix[transcript_id]["FPKM"] \
+            if transcript_id in self.abundances.index else 0
