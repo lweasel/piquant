1b343e8177783b3ec24fd83acb9efb3a5e1160ff
Owen Dando <owen.dando@ed.ac.uk>
2014-02-19 15:17:16 +0000

    Clean mapped reads, retaining only those which mapped to the correct locus

--- assess_isoform_quantification/prepare_quantification_run.py
+++ assess_isoform_quantification/prepare_quantification_run.py
@@ -32,6 +32,10 @@ BOWTIE_INDEX = "<bowtie-index>"
 FLUX_SIMULATOR_PARAMS_FILE = "flux_simulator.par"
 FLUX_SIMULATOR_PRO_FILE = "flux_simulator.pro"
 RUN_SCRIPT = "run_quantification.sh"
+TOPHAT_OUTPUT_DIR = "tho"
+
+PYTHON_SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+CLEAN_READS_SCRIPT = PYTHON_SCRIPT_DIR + os.path.sep + "clean_mapped_reads.py"
 
 # Read in command-line options
 __doc__ = __doc__.format(log_level_vals=LOG_LEVEL_VALS)
@@ -95,11 +99,21 @@ script_path = None
 
 with get_output_file(RUN_SCRIPT) as script:
     lines = [
-        "#!/bin/bash",
-        "",
+        "#!/bin/bash", ""
+    ]
+
+    # Run Flux Simulator to create expression profiles
+    lines += [
         "# Run Flux Simulator to create expression profiles then simulate reads",
         "flux-simulator -t simulator -x -p {f}".format(f=FLUX_SIMULATOR_PARAMS_FILE),
-        "",
+        ""
+    ]
+
+    # When creating expression profiles, Flux Simulator sometimes appears to
+    # output (incorrectly) one transcript with zero length - which then causes
+    # read simulation to barf. The following hack will remove the offending 
+    # transcript(s).
+    lines += [
         "# (this is a hack - Flux Simulator seems to sometimes incorrectly",
         "# output transcripts with zero length)",
         "ZERO_LENGTH_COUNT=$(awk 'BEGIN {i=0} $4 == 0 {i++;} END{print i}'" +
@@ -108,16 +122,34 @@ with get_output_file(RUN_SCRIPT) as script:
         "echo Removing $ZERO_LENGTH_COUNT transcripts with zero length...",
         "echo",
         "awk '$4 > 0' {f} > tmp; mv tmp {f}".format(f=FLUX_SIMULATOR_PRO_FILE),
-        "",
-        "flux-simulator -t simulator -l -s -p {f}".format(f=FLUX_SIMULATOR_PARAMS_FILE),
-        "",
-        "# Map simulated reads to the genome with TopHat",
-        "tophat --library-type fr-unstranded --no-coverage-search -p 8 -o tho {b} reads.fasta".format(b=BOWTIE_INDEX)
+        ""
     ]
+
+    # Now use Flux Simulator to simulate reads
+    lines += [
+        "flux-simulator -t simulator -l -s -p {f}".format(f=FLUX_SIMULATOR_PARAMS_FILE),
+        ""
+    ]
+
+    # Map simulated reads to the genome with TopHat
+    lines += [
+        "# Map simulated reads to the genome with TopHat",
+        "tophat --library-type fr-unstranded --no-coverage-search -p 8 -o {tho} {b} reads.fasta".format(tho=TOPHAT_OUTPUT_DIR, b=BOWTIE_INDEX)
+        ""
+    ]
+
+    # Clean mapped reads, retaining only those which mapped to the correct locus
+    lines += [
+        "# Clean mapped reads to retain only those which mappedi to the",
+        "# correct locus",
+        "python {s} {tho} cleaned.bam".format(s=CLEAN_READS_SCRIPT, tho=TOPHAT_OUTPUT_DIR)
+    ]
+
     write_lines(script, lines)
 
     script_path = os.path.abspath(script.name)
 
+# Make the results quantification shell script executable
 os.chmod(script_path,
          stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR |
          stat.S_IRGRP | stat.S_IROTH)
