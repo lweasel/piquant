a530525bc3bc073a7d4c8f7b8f4cc34b76ec89b1
Owen Dando <owen.dando@ed.ac.uk>
2014-04-08 14:58:09 +0100

    Add script to assemble all stats produced while varying a single variable, eg. read length for fixed quantification method, depth, single- or paired-end, and error status.

--- assess_isoform_quantification/create_results.sh
+++ assess_isoform_quantification/create_results.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 
-SINGLE_END="se"
-PAIRED_END="pe"
-WITH_ERRORS="errors"
-NO_ERRORS="no_errors"
-
-TRANSCRIPTS_GTF="$HOME/data/genome/mouse/mm10/Mus_musculus.protein_coding.gtf"
-GENOME_FASTA_DIR="$HOME/data/genome/mouse/mm10/top_level_per_contig"
-BOWTIE_INDEX="$HOME/data/genome/mouse/mm10/bowtie-index/mm10"
-TRANSCRIPT_REFERENCE="$HOME/data/genome/mouse/mm10/rsem/mm10-protein-coding"
-
-CUFFLINKS_PARAMS="BOWTIE_INDEX=${BOWTIE_INDEX}"
-RSEM_PARAMS="TRANSCRIPT_REFERENCE=${TRANSCRIPT_REFERENCE}"
-EXPRESS_PARAMS="TRANSCRIPT_REFERENCE=${TRANSCRIPT_REFERENCE}"
-
-CUFFLINKS_METHOD="Cufflinks"
-
-#METHODS="${CUFFLINKS_METHOD} RSEM Express"
-#DEPTHS="10 30 50"
-#LENGTHS="50 75 100"
-
-METHODS="Cufflinks"
-DEPTHS="30"
-LENGTHS="50 75 100"
-ENDS="${SINGLE_END} ${PAIRED_END}"
-ERRORS="${NO_ERRORS} ${WITH_ERRORS}"
+source definitions.sh
 
 for method in $METHODS;
 do
