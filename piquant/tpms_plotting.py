b1a46a575e45d8b1b5ac36d5312156a444f0b57a
Owen Dando <owen.dando@ed.ac.uk>
2014-05-09 17:05:30 +0100

    Yet more tidying of tpms.plotting.

--- piquant/tpms_plotting.py
+++ piquant/tpms_plotting.py
@@ -19,6 +19,96 @@ class _NewPlot:
         plt.close()
 
 
+def _capitalized(text):
+    return text[:1].upper() + text[1:]
+
+
+def _get_group_param_values(stats_df, group_param):
+    group_param_vals = stats_df[group_param.name].value_counts().index.tolist()
+    group_param_vals.sort()
+    return group_param_vals
+
+
+def _get_fixed_param_info(fixed_param_values):
+    return [k.get_value_name(v) for k, v in fixed_param_values.items()]
+
+
+def _get_stats_plot_name_elements(
+        base_name, plotted, group_param, fixed_param_info,
+        versus=None, ascending=None):
+    name_elements = [base_name, plotted]
+    if versus:
+        name_elements += ["vs", versus]
+    name_elements += ["per", group_param.title.lower()]
+    if ascending is not None:
+        name_elements += ("asc" if ascending else "desc")
+    name_elements += fixed_param_info
+    if ascending is not None:
+        name_elements.append("distribution")
+    return name_elements
+
+
+def _get_grouped_stats_plot_title(
+        plotted, group_param, fixed_param_info, versus=None):
+
+    title_elements = [plotted]
+    if versus:
+        title_elements += ["vs", versus.lower()]
+    title_elements += ["per", group_param.title.lower()]
+
+    return " ".join(title_elements) + ", ".join(fixed_param_info)
+
+
+def _plot_statistic_for_grouped_param_values(
+        stats_df, ycol, group_param, xcol):
+
+    group_param_vals = _get_group_param_values(stats_df, group_param)
+
+    for group_param_value in group_param_vals:
+        group_stats = stats_df[stats_df[group_param.name] == group_param_value]
+        group_stats.sort(columns=xcol, axis=0, inplace=True)
+        xvals = group_stats[xcol]
+        yvals = group_stats[ycol]
+        plt.plot(xvals, yvals, '-o',
+                 label=group_param.get_value_name(group_param_value))
+
+
+def _plot_statistic(
+        stats_df, statistic, group_param, xcol, xlabel, fixed_param_info):
+
+    _plot_statistic_for_grouped_param_values(
+        stats_df, statistic.name, group_param, xcol)
+
+    plt.xlabel(xlabel)
+    plt.ylabel(statistic.title)
+    plt.legend(title=group_param.title, loc=4)
+
+    stat_range = statistic.stat_range
+    if stat_range is not None:
+        min_val = stat_range[0]
+        max_val = stat_range[1]
+        if min_val is not None:
+            plt.ylim(ymin=min_val)
+        if max_val is not None:
+            plt.ylim(ymax=max_val)
+
+    title = _get_grouped_stats_plot_title(
+        statistic.title, group_param, fixed_param_info, versus=xlabel)
+    plt.suptitle(title)
+
+
+def _set_distribution_plot_bounds(xmin, xmax):
+    xmargin = (xmax - xmin) / 40.0
+    plt.xlim(xmin=xmin-xmargin, xmax=xmax+xmargin)
+    plt.ylim(ymin=-2.5, ymax=102.5)
+
+
+def _set_distribution_plot_labels(clsfr_col, ascending):
+    plt.xlabel(_capitalized(clsfr_col))
+    plt.ylabel("Percentage of isoforms " +
+               ("less" if ascending else "greater") + " than threshold")
+
+
 def log_tpm_scatter_plot(tpms, base_name, quant_method, tpm_label):
     with _NewPlot(base_name, tpm_label, "log10 scatter"):
         plt.scatter(tpms[t.LOG10_REAL_TPM].values,
@@ -55,68 +145,11 @@ def log_ratio_boxplot(tpms, base_name, quant_method, tpm_label, classifier,
         plt.suptitle("Log ratios of calculated to real TPMs: " +
                      ", ".join([quant_method] + name_elements))
 
-        plt.xlabel(grouping_column[:1].upper() + grouping_column[1:])
+        plt.xlabel(_capitalized(grouping_column))
         plt.ylabel("Log ratio (calculated/real TPM)")
 
         locs, labels = plt.xticks()
         plt.xticks(locs, classifier.get_value_labels(len(labels)))
-
-
-def _get_group_param_values(stats_df, group_param):
-    group_param_vals = stats_df[group_param.name].value_counts().index.tolist()
-    group_param_vals.sort()
-    return group_param_vals
-
-
-def _get_fixed_param_info(fixed_param_values):
-    return [k.get_value_name(v) for k, v in fixed_param_values.items()]
-
-
-def _get_stats_plot_name_elements(
-        base_name, statistic, versus, group_param, fixed_param_info):
-    name_elements = [base_name, statistic.name, "vs", versus,
-                     "per", group_param.title.lower()]
-    name_elements += fixed_param_info
-    return name_elements
-
-
-def _get_stats_plot_title(
-        statistic, versus, group_param, fixed_param_info):
-    title = " ".join([statistic.title, "vs", versus.lower(),
-                     "per", group_param.title.lower()])
-    title += ": " + ", ".join(fixed_param_info)
-    return title
-
-
-def _plot_stats_for_groups(
-        stats_df, statistic, group_param, xcol, xlabel, fixed_param_info):
-
-    group_param_vals = _get_group_param_values(stats_df, group_param)
-
-    for group_param_value in group_param_vals:
-        group_stats = stats_df[stats_df[group_param.name] == group_param_value]
-        group_stats.sort(columns=xcol, axis=0, inplace=True)
-        xvals = group_stats[xcol]
-        yvals = group_stats[statistic.name]
-        plt.plot(xvals, yvals, '-o',
-                 label=group_param.get_value_name(group_param_value))
-
-    plt.xlabel(xlabel)
-    plt.ylabel(statistic.title)
-    plt.legend(title=group_param.title, loc=4)
-
-    stat_range = statistic.stat_range
-    if stat_range is not None:
-        min_val = stat_range[0]
-        max_val = stat_range[1]
-        if min_val is not None:
-            plt.ylim(ymin=min_val)
-        if max_val is not None:
-            plt.ylim(ymax=max_val)
-
-    title = _get_stats_plot_title(
-        statistic, xlabel, group_param, fixed_param_info)
-    plt.suptitle(title)
 
 
 def plot_statistic(stats, base_name, statistic,
@@ -125,11 +158,11 @@ def plot_statistic(stats, base_name, statistic,
     fixed_param_info = _get_fixed_param_info(fixed_param_values)
 
     name_elements = _get_stats_plot_name_elements(
-        base_name, statistic, varying_param.name,
-        group_param, fixed_param_info)
+        base_name, statistic.name,
+        group_param, fixed_param_info, versus=varying_param.name)
 
     with _NewPlot(*name_elements):
-        _plot_stats_for_groups(
+        _plot_statistic(
             stats, statistic, group_param, varying_param.name,
             varying_param.title, fixed_param_info)
 
@@ -142,11 +175,12 @@ def plot_statistic_by_classifier(
     fixed_param_info = _get_fixed_param_info(fixed_param_values)
 
     name_elements = _get_stats_plot_name_elements(
-        base_name, statistic, clsfr_col, group_param, fixed_param_info)
+        base_name, statistic.name, group_param, fixed_param_info,
+        versus=clsfr_col)
 
     with _NewPlot(*name_elements):
-        xlabel = clsfr_col[:1].upper() + clsfr_col[1:]
-        _plot_stats_for_groups(
+        xlabel = _capitalized(clsfr_col)
+        _plot_statistic(
             stats, statistic, group_param, clsfr_col, xlabel, fixed_param_info)
 
         min_xval = stats[clsfr_col].min()
@@ -161,26 +195,17 @@ def plot_statistic_by_classifier(
 def plot_cumulative_transcript_distribution(
         tpms, base_name, quant_method, tpm_label, classifier, ascending):
 
-    xvals, yvals = t.get_distribution(tpms, classifier, ascending)
-    grouping_column = classifier.get_column_name()
+    clsfr_col = classifier.get_column_name()
 
-    with _NewPlot(base_name, grouping_column, tpm_label,
+    with _NewPlot(base_name, clsfr_col, tpm_label,
                  ("asc" if ascending else "desc"), "distribution"):
+        xvals, yvals = t.get_distribution(tpms, classifier, ascending)
         plt.plot(xvals, yvals, '-o')
 
-        plt.ylim(ymin=-2.5, ymax=102.5)
+        _set_distribution_plot_bounds(xvals[0], xvals[-1])
+        _set_distribution_plot_labels(clsfr_col, ascending)
 
-        xmin = xvals[0]
-        xmax = xvals[-1]
-        xmargin = (xmax - xmin) / 40.0
-        plt.xlim(xmin=xmin-xmargin, xmax=xmax+xmargin)
-
-        capitalized = grouping_column[:1].upper() + grouping_column[1:]
-        plt.xlabel(capitalized)
-        plt.ylabel("Percentage of isoforms " +
-                   ("less" if ascending else "greater") + " than threshold")
-
-        plt.suptitle(capitalized + " threshold: " + quant_method +
+        plt.suptitle(_capitalized(clsfr_col) + " threshold: " + quant_method +
                      ", " + tpm_label)
 
 
@@ -189,41 +214,23 @@ def plot_cumulative_transcript_distribution_grouped(
         classifier, ascending, fixed_param_values):
 
     clsfr_col = classifier.get_column_name()
-
-    group_param_values = _get_group_param_values(stats, group_param)
     fixed_param_info = _get_fixed_param_info(fixed_param_values)
 
-    name_elements = [base_name, clsfr_col, "per",
-                     group_param.title.lower(),
-                     ("asc" if ascending else "desc")]
-    name_elements += fixed_param_info
+    name_elements = _get_stats_plot_name_elements(
+        base_name, clsfr_col, group_param, fixed_param_info,
+        ascending=ascending)
 
     with _NewPlot(*name_elements):
-        for group_param_value in group_param_values:
-            group_stats = stats[stats[group_param.name] == group_param_value]
-            group_stats.sort(columns=clsfr_col, axis=0, inplace=True)
-            xvals = group_stats[clsfr_col]
-            yvals = group_stats[t.TRUE_POSITIVE_PERCENTAGE]
+        _plot_statistic_for_grouped_param_values(
+            stats, t.TRUE_POSITIVE_PERCENTAGE, group_param, clsfr_col)
 
-            plt.plot(xvals, yvals, '-o',
-                     label=group_param.get_value_name(group_param_value))
+        _set_distribution_plot_bounds(
+            stats[clsfr_col].min(), stats[clsfr_col].max())
+        _set_distribution_plot_labels(clsfr_col, ascending)
 
-        plt.ylim(ymin=-2.5, ymax=102.5)
-
-        xmin = xvals.values[0]
-        xmax = xvals.values[-1]
-        xmargin = (xmax - xmin) / 40.0
-        plt.xlim(xmin=xmin-xmargin, xmax=xmax+xmargin)
-
-        capitalized = clsfr_col[:1].upper() + clsfr_col[1:]
-        plt.xlabel(capitalized)
-        plt.ylabel("Percentage of isoforms " +
-                   ("less" if ascending else "greater") + " than threshold")
         plt.legend(title=group_param.title, loc=4)
 
-        title = " ".join([capitalized,
-                          "threshold per",
-                          group_param.title.lower()]) + \
-            ": " + ", ".join(fixed_param_info)
-
+        title = _get_grouped_stats_plot_title(
+            _capitalized(clsfr_col) + "threshold", group_param,
+            fixed_param_info)
         plt.suptitle(title)
