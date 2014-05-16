fee272110e493b6a52e88a610d04525e35c73a41
Owen Dando <owen.dando@ed.ac.uk>
2014-05-16 15:19:55 +0100

    Allow one-time preparatory commands for quantifiers to be executed separately from quantification itself. Fixes #1.

--- piquant/run_quantifiers.py
+++ piquant/run_quantifiers.py
@@ -3,7 +3,9 @@
 # TODO: should be able to separately specify parent directory for reads directories.
 
 """Usage:
-    run_quantifiers [--log-level=<log-level>] [--out-dir=<out-dir>] [--num-fragments=<num-fragments>] [--prepare-only|--run-only] --quant-method=<quant-methods> --read-length=<read-lengths> --read-depth=<read-depths> --paired-end=<paired-ends> --error=<errors> --bias=<biases> --polya=<polya> <transcript-gtf-file> <genome-fasta-dir>
+    run_quantifiers prepare [--log-level=<log-level>] [--out-dir=<out-dir>] [--num-fragments=<num-fragments>] --quant-method=<quant-methods> --read-length=<read-lengths> --read-depth=<read-depths> --paired-end=<paired-ends> --error=<errors> --bias=<biases> --polya=<polya> <transcript-gtf-file> <genome-fasta-dir>
+    run_quantifiers prequantify [--log-level=<log-level>] [--out-dir=<out-dir>] [--num-fragments=<num-fragments>] --quant-method=<quant-methods> --read-length=<read-lengths> --read-depth=<read-depths> --paired-end=<paired-ends> --error=<errors> --bias=<biases> --polya=<polya> <transcript-gtf-file> <genome-fasta-dir>
+    run_quantifiers quantify [--log-level=<log-level>] [--out-dir=<out-dir>] [--num-fragments=<num-fragments>] --quant-method=<quant-methods> --read-length=<read-lengths> --read-depth=<read-depths> --paired-end=<paired-ends> --error=<errors> --bias=<biases> --polya=<polya> <transcript-gtf-file> <genome-fasta-dir>
 
 -h --help                          Show this message.
 -v --version                       Show version.
@@ -24,7 +26,7 @@
 """
 
 from docopt import docopt
-from schema import Schema, SchemaError
+from schema import SchemaError
 
 import ordutils.log as log
 import ordutils.options as opt
@@ -40,8 +42,9 @@ LOG_LEVEL = "--log-level"
 LOG_LEVEL_VALS = str(log.LEVELS.keys())
 OUTPUT_DIRECTORY = "--out-dir"
 NUM_FRAGMENTS = "--num-fragments"
-PREPARE_ONLY = "--prepare-only"
-RUN_ONLY = "--run-only"
+PREPARE = "prepare"
+PREQUANTIFY = "prequantify"
+QUANTIFY = "quantify"
 TRANSCRIPT_GTF_FILE = "<transcript-gtf-file>"
 GENOME_FASTA_DIR = "<genome-fasta-dir>"
 
@@ -71,6 +74,8 @@ try:
 except SchemaError as exc:
     exit("Exiting. " + exc.code)
 
+quantifiers_used = []
+
 if False in param_values[parameters.PAIRED_END]:
     for qm in param_values[parameters.QUANT_METHOD]:
         if qm.requires_paired_end_reads():
@@ -90,17 +95,26 @@ def check_reads_directory(**params):
 def check_run_directory(**params):
     run_dir = options[OUTPUT_DIRECTORY] + os.path.sep + \
         parameters.get_file_name(**params)
-    if options[RUN_ONLY] != os.path.exists(run_dir):
+    should_exist = options[PREQUANTIFY] or options[QUANTIFY]
+    if should_exist != os.path.exists(run_dir):
         sys.exit("Run directory '{d}' should ".format(d=run_dir) +
-                 ("" if options[RUN_ONLY] else "not ") + "already exist.")
+                 ("" if should_exist else "not ") + "already exist.")
+
+
+def execute_quantification_script(run_dir, cl_opts):
+    cwd = os.getcwd()
+    os.chdir(run_dir)
+    args = ['nohup', './run_quantification.sh', cl_opts]
+    subprocess.Popen(args)
+    os.chdir(cwd)
 
 
 def create_and_run_quantification(**params):
     run_dir = options[OUTPUT_DIRECTORY] + os.path.sep + \
         parameters.get_file_name(**params)
 
-    # Create the run quantification script
-    if not options[RUN_ONLY]:
+    if options[PREPARE]:
+        # Create the run quantification script
         reads_params = dict(params)
         del reads_params[parameters.QUANT_METHOD]
         reads_dir = options[OUTPUT_DIRECTORY] + os.path.sep + \
@@ -116,15 +130,18 @@ def create_and_run_quantification(**params):
         prq.write_run_quantification_script(
             reads_dir, run_dir, quantifier_dir, options[TRANSCRIPT_GTF_FILE],
             params_spec, **params)
-
-    # Execute the run quantification script
-    if not options[PREPARE_ONLY]:
+    elif options[PREQUANTIFY]:
+        # Execute prequantification commands, but only once per quantifier
+        quant_method = param_values[parameters.QUANT_METHOD]
+        if quant_method not in quantifiers_used:
+            quantifiers_used.append(quant_method)
+            logger.info("Executing prequantification for " +
+                        quant_method.get_name())
+            execute_quantification_script(run_dir, "-p")
+    elif options[QUANTIFY]:
+        # Execute the run quantification script
         logger.info("Executing shell script to run quantification analysis.")
-        cwd = os.getcwd()
-        os.chdir(run_dir)
-        args = ['nohup', './run_quantification.sh', "-pqa"]
-        subprocess.Popen(args)
-        os.chdir(cwd)
+        execute_quantification_script(run_dir, "-qa")
 
 
 # Set up logger
