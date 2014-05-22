eb4a66480c9f2eee8a983e0da5a9535d7510cfff
Owen Dando <owen.dando@ed.ac.uk>
2014-05-22 10:36:43 +0100

    Remove some unused code.

--- piquant/flux_simulator.py
+++ piquant/flux_simulator.py
@@ -1,24 +1,10 @@
 import file_writer as fw
 import pandas as pd
 
-# n.b. a Flux Simulator read identifier takes a form like:
-#
-# 17:23917458-23919441W:ENSMUST00000115428:3404:610:576:768:S
-#
-# Here "17" is the region, "23917458" and 23919441 are the start and end
-# positions of the originating transcript (both in 1-based co-ordinates),
-# "610" is the length of the processed transcript, "576" is the start
-# position of the fragment relative to the transcript and "768" is the
-# end position of the fragment relative to the transcript.
-
-PRO_FILE_LOCUS_COL = 0
 PRO_FILE_TRANSCRIPT_ID_COL = 1
-PRO_FILE_CODING_COL = 2
 PRO_FILE_LENGTH_COL = 3
 PRO_FILE_FRAC_COL = 4
 PRO_FILE_NUM_COL = 5
-PRO_FILE_LIBRARY_FRAC_COL = 6
-PRO_FILE_LIBRARY_NUM_COL = 7
 PRO_FILE_SEQ_FRAC_COL = 8
 PRO_FILE_SEQ_NUM_COL = 9
 PRO_FILE_COV_FRAC_COL = 10
@@ -26,27 +12,20 @@ PRO_FILE_CHI_SQR_COL = 11
 PRO_FILE_VAR_COEFF_COL = 12
 
 PRO_FILE_COLS = [
-    PRO_FILE_LOCUS_COL,
+    0,
     PRO_FILE_TRANSCRIPT_ID_COL,
-    PRO_FILE_CODING_COL,
+    2,
     PRO_FILE_LENGTH_COL,
     PRO_FILE_FRAC_COL,
     PRO_FILE_NUM_COL,
-    PRO_FILE_LIBRARY_FRAC_COL,
-    PRO_FILE_LIBRARY_NUM_COL,
+    6,
+    7,
     PRO_FILE_SEQ_FRAC_COL,
     PRO_FILE_SEQ_NUM_COL,
     PRO_FILE_COV_FRAC_COL,
     PRO_FILE_CHI_SQR_COL,
     PRO_FILE_VAR_COEFF_COL
 ]
-
-REGION_READ_ELEMENT = 0
-LOCUS_READ_ELEMENT = 1
-TRANSCRIPT_ID_READ_ELEMENT = 2
-LENGTH_READ_ELEMENT = 4
-START_POS_READ_ELEMENT = 5
-END_POS_READ_ELEMENT = 6
 
 EXPRESSION_PARAMS_FILE = "flux_simulator_expression.par"
 SIMULATION_PARAMS_FILE = "flux_simulator_simulation.par"
@@ -62,67 +41,6 @@ def read_expression_profiles(pro_file):
                            header=None, names=PRO_FILE_COLS)
     #profiles.set_index(PRO_FILE_TRANSCRIPT_ID_COL, inplace=True)
     return profiles
-
-
-def get_read_identifier_elems(read_identifier):
-    return read_identifier.split(":")
-
-
-def strip_orientation_info(rid_elems):
-    """Return read identifier minus the orientation information."""
-    return ":".join(rid_elems[:-1])
-
-
-def get_transcript_id(rid_elems):
-    """Return originating transcript ID for a Flux Simulator read."""
-    return rid_elems[TRANSCRIPT_ID_READ_ELEMENT]
-
-
-def get_transcript_length(rid_elems):
-    """Return the processed length of the originating transcript of a read"""
-    return int(rid_elems[LENGTH_READ_ELEMENT])
-
-
-def get_transcript_bounds(read_identifier):
-    """Return transcript bounds for a Flux Simulator read.
-
-    Along with the region of the originating transcript, this function
-    will return (in 0-based, half-open co-ordinates) the start and end
-    positions of the read's originating transcript."""
-    rid_elems = get_read_identifier_elems(read_identifier)
-    region = rid_elems[REGION_READ_ELEMENT]
-    start_str, end_str = rid_elems[LOCUS_READ_ELEMENT][:-1].split("-")
-    return region, int(start_str) - 1, int(end_str)
-
-
-def get_fragment_bounds(read_identifier):
-    """Return fragment bounds for a Flux Simulator read.
-
-    This function behaves the same as get_transcript_bounds above, unless:
-    i)  The fragment start position relative to the transcript is negative,
-        in which case the bounds will be extended at the start to accommodate
-        this, or
-    ii) The fragment end position relative to the transcript is beyond the
-        end of the transcript (i.e. end pos >= len), in which case the bounds
-        will be extended at the end to accommodate this.
-    """
-    rid_elems = get_read_identifier_elems(read_identifier)
-    region = rid_elems[REGION_READ_ELEMENT]
-
-    t_start_str, t_end_str = rid_elems[LOCUS_READ_ELEMENT][:-1].split("-")
-    t_start = int(t_start_str) - 1
-    t_end = int(t_end_str)
-    t_len = int(rid_elems[LENGTH_READ_ELEMENT])
-
-    f_start = int(rid_elems[START_POS_READ_ELEMENT])
-    if f_start < 0:
-        t_start += f_start
-
-    f_end = int(rid_elems[END_POS_READ_ELEMENT])
-    if f_end >= t_len:
-        t_end += f_end - t_len + 1
-
-    return region, t_start, t_end
 
 
 def _get_common_flux_simulator_params(
@@ -142,6 +60,7 @@ def _write_flux_simulator_expression_params(
 
     fs_params = _get_common_flux_simulator_params(
         transcript_gtf_file, genome_fasta_dir, num_fragments)
+
     writer = fw.FluxSimulatorParamsWriter(fs_params)
     writer.write_to_file(output_dir, EXPRESSION_PARAMS_FILE)
 
