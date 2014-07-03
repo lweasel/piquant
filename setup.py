fce6a3b432818091bcce98a667b3fec4fc7618cc
Owen Dando <owen.dando@ed.ac.uk>
2014-07-03 10:41:33 +0100

    Unique project name (for PyPI).

--- setup.py
+++ setup.py
@@ -20,12 +20,13 @@ class PyTest(TestCommand):
         sys.exit(errcode)
 
 setup(
-    name='piquant',
+    name='piquant-pipeline',
     version=piquant.__version__,
+    packages=['piquant'],
+    license='MIT License',
     description='Pipeline for investigating the quantification of transcripts',
     author="Owen Dando",
     author_email='owen.dando@ed.ac.uk',
-    packages=['piquant'],
     install_requires=['docopt>=0.6.1',
                       'py>=1.4.20',
                       'pytest>=2.5.2',
