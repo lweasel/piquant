c82092af0896b28b0bccf52a0bc97479db251ce8
Owen Dando <owen.dando@ed.ac.uk>
2014-08-29 12:57:28 +0100

    Fix script structure of calculate_reads_for_depth.

--- piquant/calculate_reads_for_depth.py
+++ piquant/calculate_reads_for_depth.py
@@ -25,49 +25,59 @@ PRO_FILE = "<pro-file>"
 READ_LENGTH = "<read-length>"
 READ_DEPTH = "<read-depth>"
 
-# Read in command-line options
-__doc__ = __doc__.format(log_level_vals=LOG_LEVEL_VALS)
-options = docopt.docopt(__doc__, version="calculate_reads_for_depth v0.1")
 
-# Validate and process command-line options
+def _validate_command_line_options(options):
+    try:
+        opt.validate_dict_option(
+            options[LOG_LEVEL], log.LEVELS, "Invalid log level")
+        opt.validate_file_option(
+            options[PRO_FILE], "Could not open expression profile file")
+        options[READ_LENGTH] = opt.validate_int_option(
+            options[READ_LENGTH], "Read length must be a positive integer",
+            nonneg=True)
+        options[READ_DEPTH] = opt.validate_int_option(
+            options[READ_DEPTH], "Read depth must be a positive integer",
+            nonneg=True)
+    except schema.SchemaError as exc:
+        exit("Exiting. " + exc.code)
 
-try:
-    opt.validate_dict_option(
-        options[LOG_LEVEL], log.LEVELS, "Invalid log level")
-    opt.validate_file_option(
-        options[PRO_FILE], "Could not open expression profile file")
-    options[READ_LENGTH] = opt.validate_int_option(
-        options[READ_LENGTH], "Read length must be a positive integer",
-        nonneg=True)
-    options[READ_DEPTH] = opt.validate_int_option(
-        options[READ_DEPTH], "Read depth must be a positive integer",
-        nonneg=True)
-except schema.SchemaError as exc:
-    exit("Exiting. " + exc.code)
 
-# Set up logger
-logger = log.get_logger(sys.stderr, options[LOG_LEVEL])
+def _read_expression_profiles(logger, pro_file):
+    logger.info("Reading expression profiles from '{f}'...".format(f=pro_file))
+    profiles = fs.read_expression_profiles(pro_file)
+    profiles.set_index(fs.PRO_FILE_TRANSCRIPT_ID_COL, inplace=True)
+    logger.info("...read profiles for {n} transcripts.".
+                format(n=len(profiles)))
 
-# Read in Flux Simulator expression profiles
-logger.info("Reading expression profiles from '{f}'...".
-            format(f=options[PRO_FILE]))
+    # Filter those transcripts with zero expression in the profile file
+    profiles = profiles[profiles[fs.PRO_FILE_NUM_COL] > 0]
+    logger.info("Retained {n} transcripts with non-zero expression.".
+                format(n=len(profiles)))
 
-profiles = fs.read_expression_profiles(options[PRO_FILE])
-profiles.set_index(fs.PRO_FILE_TRANSCRIPT_ID_COL, inplace=True)
+    return profiles
 
-logger.info("...read profiles for {n} transcripts.".
-            format(n=len(profiles)))
 
-# Filter those transcripts with zero expression in the profile file
-profiles = profiles[profiles[fs.PRO_FILE_NUM_COL] > 0]
+def _calculate_reads_for_depth(profiles, read_length, required_depth):
+    total_transcript_length = profiles[fs.PRO_FILE_LENGTH_COL].sum()
+    bases_to_sequence = total_transcript_length * required_depth
+    return bases_to_sequence // read_length
 
-logger.info("Retained {n} transcripts with non-zero expression.".
-            format(n=len(profiles)))
 
-# Output the required number of reads to (approximately) give the specified
-# read depth
-total_transcript_length = profiles[fs.PRO_FILE_LENGTH_COL].sum()
-bases_to_sequence = total_transcript_length * options[READ_DEPTH]
-num_reads = bases_to_sequence // options[READ_LENGTH]
+if __name__ == "__main__":
+    # Read in command-line options
+    __doc__ = __doc__.format(log_level_vals=LOG_LEVEL_VALS)
+    options = docopt.docopt(__doc__, version="calculate_reads_for_depth v0.1")
 
-print(num_reads)
+    # Validate and process command-line options
+    _validate_command_line_options(options)
+
+    # Set up logger
+    logger = log.get_logger(sys.stderr, options[LOG_LEVEL])
+
+    # Read in Flux Simulator expression profiles
+    profiles = _read_expression_profiles(logger, options[PRO_FILE])
+
+    # Calculate the number of reads required to approximately give the
+    # specified overall average depth of coverage
+    print(_calculate_reads_for_depth(
+        profiles, options[READ_LENGTH], options[READ_DEPTH]))
