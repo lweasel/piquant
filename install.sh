87e36d340088e66c09dc0865272f612ccaf9925f
Owen Dando <owen.dando@ed.ac.uk>
2017-05-18 11:09:51 +0100

    Propitiate the angry gods of Python packaging.
    For some reason matplotlib will no longer install correctly via pip - it
    appears to download and install numpy itself in order to have this to build
    against, but the version it downloads is not the correct version that it later
    seems to require for installation (??). Have added an installation script to
    ensure that the version of numpy that we desire is installed prior to
    matplotlib being installed, since this ordering does not appear to be
    achievable through setup.py itself.

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install 'numpy==1.10.4'
+pip install -e .
--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
         'docopt==0.6.1',
         'docutils==0.11',
         'matplotlib==1.4.1',
-        'numpy==1.10.4',
         'pandas==0.13.0',
         'py==1.4.20',
         'pyparsing==2.0.2',
